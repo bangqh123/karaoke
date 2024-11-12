21d7017e18dcf83876cde154759615ed8167323a
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-12 20:08:53 +0700

    Build source

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/_globals.style.scss";
+          @import "./src/styles/_variables.style.scss";
+        `,
+      },
+    },
+  },
+  server: {
+    open: true,
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
+})
