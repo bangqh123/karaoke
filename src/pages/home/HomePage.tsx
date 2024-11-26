4ea3dd2c014a6f79649d6f0fb377204225399088
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-26 19:09:06 +0700

    setup

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import "./HomePage.less"
+
+const HomePage = () => {
+    return (
+        <div className="adsd">
+            dsdsd
+            <div className="sdds">dsds</div>
+        </div>
+    )
+}
+
+export default HomePage;
